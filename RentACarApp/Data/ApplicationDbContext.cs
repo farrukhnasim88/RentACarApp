2010e17eee8ad3e5f49cc1bfea7523cbf69e0bfb
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-07-02 09:56:39 +1000

    Add project files.

---
+++ RentACarApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACarApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+         
+
+        public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options ): base(options)
+        {
+                
+        }
+
+
+        public DbSet <Vehicle> Vehicles { get; set; }
+
+
+
+
+
+    }
+}
