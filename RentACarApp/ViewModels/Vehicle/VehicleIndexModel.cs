98ffa26f9acff23e76ad0605cd3a4dfe96887e38
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-09-16 14:04:49 +1000

    added view model index

---
+++ RentACarApp/ViewModels/Vehicle/VehicleIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RentACarApp.ViewModels.Vehicle
+{
+    public class VehicleIndexModel
+    {
+        public IEnumerable<VehiclesViewModel> Vehicles { get; set; }
+    }
+}
